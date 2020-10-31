bf098cef6f777aed810c64ff5811eca8aee89a2d
momona <momonasky@gmail.com>
2020-10-31 16:54:19 +0900

    no message

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      for ($i = 1; $i <= 10; $i++) {
+            User::create([
+                'username'    => 'test_user' .$i,
+                'mail'          => 'test' .$i .'@test.com',
+                'password'       => Hash::make('12345678'),
+                'created_at'     => now(),
+                'updated_at'     => now()
+            ]);
+        }
+    }
+}
