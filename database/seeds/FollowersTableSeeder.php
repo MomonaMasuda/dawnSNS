f2fd748c734e0d5fceeb6508f8e433c8a76d21a3
momona <momonasky@gmail.com>
2020-11-14 12:22:04 +0900

    no message

---
+++ database/seeds/FollowersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Follow;
+
+class FollowersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      for ($i = 2; $i <= 10; $i++) {
+          Follow::create([
+              'follow' => $i,
+              'follower' => 1,
+              'created_at' => now(),
+              'updated_at' => now()
+          ]);
+    }
+}
+}
